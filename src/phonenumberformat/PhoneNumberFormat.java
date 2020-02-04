58b389e9f9529570336706f90dbc9d431f26031b
amiyasmith24@gmail.com <>
2020-02-04 12:23:39 -0500

    this is the first commit

---
+++ src/phonenumberformat/PhoneNumberFormat.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phonenumberformat;
+
+/**
+ *
+ * @author amiya
+ */
+import java.util.Scanner;
+public class PhoneNumberFormat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+        System.out.print("Please enter a ten-digit phone number: ");
+        String n = in.next();
+
+        String areaCode = "(" + n.substring(0, 3) + ") ";
+        String formattedNumber = areaCode + n.substring(3, 6) + "-" + n.substring(6);
+        System.out.println("Formatted number: " + formattedNumber);
+    }
+    
+}
